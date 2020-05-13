d2677cd1082c4de31b6ec0f1ea9a0b5023199e75
Abdul Khan <akhan17@outlook.com>
2020-05-13 19:50:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,7 +22,7 @@ class InteractiveRecord
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
-      self.id ||= nil
+      self.id = nil
     end
   end
 
